8a6882d3f225e95882842ff09c26c1af08d255c7
ReneJ <milogtorte@gmail.com>
2022-12-11 14:52:17 +0100

    MOIN NIKOLIS

--- src/main/java/mod/milog/theonetest/ModEventSubscriber.java
+++ src/main/java/mod/milog/theonetest/ModEventSubscriber.java
@@ -15,7 +15,7 @@ public final class ModEventSubscriber {
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(
-            new Item[]{Items.SEA_SALT_STUFF,Items.KATANA,Items.BLOOD_STONE,Items.LASER,Items.LASER_GUN,Items.PORTAL_GUN_1,Items.PORTAL_GUN_2,Items.BLOOD_ORE,Items.FIRE_DRAGON_STAFF}
+            new Item[]{Items.SEA_SALT_STUFF,Items.KATANA,Items.BLOOD_STONE,Items.LASER,Items.LASER_GUN,Items.PORTAL_GUN_1,Items.PORTAL_GUN_2,Items.BLOOD_ORE,Items.FIRE_DRAGON_STAFF,Items.BLUE_BERRY,Items.BLUE_BERRY_PIE,Items.SWORD_OF_DAYLIGHT,Items.SWORD_OF_ECLIPSE,Items.SWORD_BASE,Items.SWORD_UNIVERSE,Items.SWORD_HEAL,Items.SWORD_EMOTION,Items.SWORD_FOOL,Items.SWORD_STRONG,Items.SWORD_SUN,Items.SWORD_AGILITY,Items.DOUBLE_SWORD,Items.AGILITY_STONE,Items.EMOTION_STONE,Items.FOOL_STONE,Items.HEAL_STONE,Items.SHADOW_STONE,Items.STRONG_STONE,Items.SUN_STONE,Items.UNIVERSE_STONE,Items.SWORD_SHADOW,Items.AGILITY_SHARD,Items.EMOTION_SHARD,Items.FOOL_SHARD,Items.HEAL_SHARD,Items.SHADOW_SHARD,Items.STRONG_SHARD,Items.SUN_SHARD,Items.UNIVERSE_SHARD}
         );
 
     }
